c442c7c30ae4d23f21fff2b16ca9aec8eabcc00b
LCruzFer <lucas.cruz@hotmail.de>
2021-09-29 13:55:18 +0200

    kmeans liquidity and adjust prep data

---
+++ src/analysis/kmeans_liquidity.py
@@ -0,0 +1,127 @@
+import sys 
+from pathlib import Path 
+import pandas as pd 
+import numpy as np
+from sklearn.cluster import KMeans 
+from sklearn.preprocessing import StandardScaler 
+from sklearn.metrics import silhouette_score
+import matplotlib.pyplot as plt
+
+#* set system path to import utils
+wd=Path.cwd()
+#* set data paths
+data_in=wd.parents[1]/'data'/'in'
+data_out=wd.parents[1]/'data'/'out'
+fig_out=wd.parents[1]/'figures'
+
+#*#########################
+#! FUNCTIONS
+#*#########################
+def group_plot(xaxis, yaxis, groups, xlab='Month', ylab='Liquidity', title='Changes in liquidity within groups'): 
+    '''
+    Plot y axis for different groups in same plot.
+    '''
+    fig, ax=plt.subplots()
+    for i in groups:
+        ax.plot(xaxis, yaxis[i], label=f'Group {i+1}')
+    ax.set_xlabel(xlab)
+    ax.set_ylabel(ylab)
+    ax.set_title(title)
+    ax.legend(bbox_to_anchor=(1, 1), loc='upper left')
+    
+    return fig, ax
+
+#*#########################
+#! DATA
+#*#########################
+#read in raw data for first quarter of 2008
+#additionally second quarter to compare to April (buffer time)
+raw081=pd.read_csv(data_in/'2008_rawdata'/'intrvw08'/'fmli081x.csv')
+raw082=pd.read_csv(data_in/'2008_rawdata'/'intrvw08'/'fmli082.csv')
+raw=raw081.append(raw082)
+#only use households that have not yet received their rebate (!)
+#load rebate data 
+rbt08=pd.read_csv(data_in/'2008_rawdata'/'expn08'/'rbt08.csv')
+#get IDs of households that already received their rebate
+rbtrcvd=rbt08[rbt08['RBTMO']<5]['NEWID']
+#only keep relevant variables that we have in main data 
+variables=['NEWID', 'QINTRVYR', 'QINTRVMO', 'AGE_REF', 'SAVACCTX', 'MARITAL1', 'CKBKACTX', 'FINCBTXM', 'FSALARYM', 'CUTENURE', 'ST_HOUS', 'PERSLT18', 'AGE2', 'FAM_SIZE']
+#not in fmli: 'QESCROWX', 'QMRTTERM', 'ORGMRTX', 'QBLNCM1X', 'NEWMRRT'  
+raw_vars=raw[variables]
+#drop irrelevant months 
+months=[1, 2, 3, 4]
+relevant=raw_vars[raw_vars['QINTRVMO'].isin(months)]
+#and only keep households that have not yet received rebate
+relevant=relevant[relevant['NEWID'].isin(rbtrcvd)==False]
+#drop rows that have NA 
+relevant=relevant.replace('.', np.nan)
+relevant['SAVACCTX']=relevant['SAVACCTX'].astype(float)
+relevant['CKBKACTX']=relevant['CKBKACTX'].astype(float)
+#also AGE2 is a string
+relevant['AGE2']=relevant['AGE2'].astype(float)
+relevant['AGE_REF']=relevant['AGE_REF'].astype(float)
+#AGE=mean(AGE+AGE2) if AGE2!=np.nan or 0 
+relevant['AGE']=relevant['AGE_REF']
+relevant['AGE'][(relevant['AGE2']!=np.nan)|(relevant['AGE2']!=0)]=(relevant['AGE2']+relevant['AGE_REF'])/2
+relevant=relevant.drop(['AGE2', 'AGE_REF'], axis=1)
+relev_nona=relevant.dropna()
+relev_nona['liqassii']=relev_nona['SAVACCTX']+relev_nona['CKBKACTX']
+
+#for grouping ignore liquidity, year is 2008 anyway and nor relevant 
+df=relev_nona.drop(['SAVACCTX', 'CKBKACTX', 'QINTRVYR'], axis=1).set_index(['NEWID', 'QINTRVMO'], drop=True)
+liquid_df=df[['liqassii']]
+df=df.drop(['liqassii'], axis=1)
+#scale everything 
+scaler=StandardScaler()
+scaled=scaler.fit_transform(df)
+
+#*#########################
+#! ANALYSIS
+#*#########################
+#* Apply k means algorithm
+#set number of clusters
+n_clust=5
+#set up Kmeans
+kmeans=KMeans(init='random', n_clusters=n_clust, n_init=10, max_iter=300, random_state=42)
+#fit K-means algorithm 
+kmeans.fit(scaled)
+#get group assignment for each observation
+liquid_df['group']=kmeans.labels_
+#get group-month averages of liquidity
+liquid_df=liquid_df.reset_index(level='QINTRVMO')
+liqavg=liquid_df.groupby(['group', 'QINTRVMO']).mean().reset_index()
+
+#* Plot 
+#get array with group numbers
+groups=np.linspace(start=0, stop=n_clust-1, num=n_clust, dtype=int)
+#set months for x axis
+months=[1, 2, 3, 4]
+x_axis=np.array(months)
+#then get y_axis 
+y_axis=np.array([liqavg['liqassii'][liqavg['group']==i] for i in groups])
+#then plot group
+group_plot(x_axis, y_axis, groups=groups)
+
+#* Group means/medians
+#what are group means and medians? 
+df=df.reset_index(level='QINTRVMO')
+df['group']=kmeans.labels_
+group_medians=df.groupby(['group']).median()
+group_means=df.groupby(['group']).mean()
+group_count=df.groupby(['group']).count().rename(columns={'QINTRVMO': 'count'})['count']
+
+#* Get correlation with increase/decrease 
+liqavg['increase']=0
+liqavg.loc[:3, 'increase']=1
+liqavg.loc[4:7, 'increase']=0
+liqavg.loc[8:11, 'increase']=1
+liqavg.loc[12:15, 'increase']=1
+liqavg.loc[16:, 'increase']=1
+liq_increase=liqavg.drop_duplicates(subset=['group', 'increase'])
+#merge with data df 
+df=df.merge(liq_increase, on='group', how='left')
+
+observables=['FINCBTXM', 'FSALARYM', 'CUTENURE', 'ST_HOUS', 'PERSLT18', 'AGE', 'FAM_SIZE']
+corr={}
+for var in observables:
+    corr[var]=np.corrcoef(df['increase'], df[var])
--- src/analysis/prep_data.py
+++ src/analysis/prep_data.py
@@ -1,20 +1,36 @@
-import sys
-from pathlib import Path
-import pandas as pd
-import numpy as np
+import sys 
+from pathlib import Path 
+import pandas as pd 
+import numpy as np 
 
-#* get data paths 
+#* set system path to import utils
 wd=Path.cwd()
+sys.path.append(str(wd.parent))
+
+#* set data paths
 data_in=wd.parents[1]/'data'/'in'
 data_out=wd.parents[1]/'data'/'out'
-
 '''
-This file prepares the dataset for the dynamic DML estimation.
+#TODO 
+1. keep only relevant variables []
+2. transform categoricals into dummy variables []
+3. ???
 '''
 
 #*#########################
 #! FUNCTIONS
 #*#########################
+def gen_samp_dummy(df, var, idvar='newid'): 
+    '''
+    Generate a dummy that signals whether variable is in sample where var is not NAN.
+    
+    *df=pd DataFrame 
+    *var=str; column name in df on which basis sample is generated
+    *idvar=str; column name in df that signals id 
+    '''
+    dummies=(df[idvar].isin(df[df[var].isnull()==False][idvar])).astype(int)
+    return dummies
+
 def reorder_df(columns, df): 
     '''
     Make columns the first columns of df in order they are contained in columns. 
@@ -44,147 +60,119 @@ def create_dummies(df, id_col):
     
     return dummy_df
 
-def get_expnames(df): 
-    '''
-    Get column names of all expenditure variables, split into their level, change and lag versions.
-    *df=pandas DataFrame
-    '''
-    #get columns that have 'exp' in their name
-    exp_cols=[col for col in df.columns if 'exp' in col]
-    #also filter between level, change and lag 
-    lvl=[col for col in exp_cols if ('ch' not in col) & ('last' not in col)]
-    ch=[col for col in exp_cols if 'ch' in col]
-    lag=[col for col in exp_cols if 'last' in col]
-    return lvl, ch, lag
-
-def create_dummies(df, id_col):
-    '''
-    Transform all columns except id_col into type categorical.
-    
-    df=pandas.DataFrame with columns containing categorical variables 
-    id_col=column with observation id that is not transformed, must be unique for each row
-    '''
-    #set id as index such that ignored in transformation
-    df=df.set_index(id_col)
-    #declare all columns as type categorical
-    cat_df=df.astype('category')
-    #create dummy df with pd.get_dummies() 
-    dummy_df=pd.get_dummies(cat_df)
-    #turn index into column again 
-    dummy_df=dummy_df.reset_index()
-    
-    return dummy_df
-
 #*#########################
 #! DATA
 #*#########################
-#use Misra & Surico data
+#read in data 
 ms_data=pd.read_csv(data_in/'Misra_Surico_Data'/'2008_data.csv')
-#reorder this dataframe to make it easier to read 
-ms_data=reorder_df(['custid', 'interviewno', 'newid', 'QINTRVMO', 'QINTRVYR'], ms_data)
-#load parker et al data 
-parkeretal=pd.read_stata(data_in/'Parkeretal_Data.dta')
-parkeretal['newid']=[int(str(x)+str(y)) for x, y in zip(parkeretal['newid'], parkeretal['intview'])]
-#merge some variables from Parker et al that are not included in MS 
-ms_data=ms_data.merge(parkeretal[['newid', 'liqassii']], on='newid')
-#replace NaN with 0 
-ms_data=ms_data.replace(np.nan, 0)
+#also read in csv for sample dummies
+parker=pd.read_stata(data_in/'Parkeretal_Data.dta')
+parker['newid']=(parker['newid'].astype(str)+parker['intview'].astype(str)).astype(int)
+#then merge 
+ms_data=ms_data.merge(parker[['newid', 'samplemain', 'sample2', 'sample2a', 'sample3', 'sample4', 'sample', 'sample4l2i']], on='newid', how='left')
+#age and children are included twice - keep MS version 
+ms_data=ms_data.drop(['AGE_REF', 'PERSLT18'], axis=1)
+#load parker et al data for the liquid assets data 
+parker=pd.read_stata(data_in/'Parkeretal_Data.dta')
+parker['newid']=(parker['newid'].astype(str)+parker['intview'].astype(str)).astype(int)
+ms_data=ms_data.merge(parker[['newid', 'liqassii']], on='newid')
 
-#* General cleaning
-#drop variables that are included twice - keep MS version because of better naming
-nodoubles=ms_data.drop(['AGE_REF', 'PERSLT18'], axis=1)
-#rename variables to names that are better understandable
-newnames={'CKBK_CTX': 'totbalance_ca', 'FINCBTXM': 'tot_fam_inc', 
-            'FSALARYM': 'fam_salary_inc', 'MARITAL1': 'maritalstat', 
-            'SAVA_CTX': 'totbalance_sa', 'QINTRVMO': 'month', 
-            'QINTRVYR': 'year', 'QBLNCM1X': 'out_princ'}
-nodoubles=nodoubles.rename(columns=newnames)
-#drop variables that are not expenditure, rebate, ID or characteristics 
-#also drop ST_HOUS because all are ==2 (not student housing)
-#validIncome and validAssets bear no useful information on their own (for now)
-#!what is nmort?
-cleaned=nodoubles.drop(['RESPSTAT', 'timetrend', 'dropconsumer',
-                            'nmort', 'const', 'dropcust', 'timeleft', 
-                            'lasttimetrend', 'chtimetrend', 'ST_HOUS', 
-                            'validIncome', 'validAssets']
-                            + ['const'+str(i) for i in range(15)], axis=1)
-#also drop the rebate variables that have no meaning and the indicators 
-#latter will be created if needed later on by myself again
-#!maybe keep them later on if I know what those actually represent
-cleaned=cleaned.drop(['iontimereb', 'ireballt', 'ireballt_CHK', 
-                            'iREB', 'iontimereb_CHK', 'iontimereb_EF', 
-                            'ilreb', 'ifreb', 'futrebamt'], axis=1)
+#* Variable Lists
+#most important observables
+observables=['AGE', 'SAVA_CTX', 'MARITAL1', 'CKBK_CTX', 'QESCROWX', 'QMRTTERM', 'ORGMRTX', 'FINCBTXM', 'FSALARYM', 'liqassii', 'adults', 'children', 'FAM_SIZE', 'QBLNCM1X', 'NEWMRRT', 'CUTENURE', 'ST_HOUS']
+#ID and indicator variables 
+meta=['custid', 'newid', 'QINTRVYR', 'QINTRVMO', 'interviewno']
+#treatment variables 
+treatment=['RBTAMT', 'RBTAMT_CHK', 'RBTAMT_EF', 'ontime', 'iREB', 'ontimeRBTAMT', 'ireballt', 'ireballt_CHK', 'iontimereb', 'iontimereb_CHK', 'iontimereb_EF', 'lastrbtamt', 'futrebamt', 'ilreb', 'ifreb']
+#dependent variables (change in expenditure)
+chexpvars=[col for col in ms_data.columns if ('exp' in col)&('ch' in col)]
+#lagged variables
+lastvars=[col for col in ms_data.columns if 'last' in col]
+#contemporaneous expenditure 
+expvars=[col for col in ms_data.columns if ('exp' in col)&('ch' not in col)&('last' not in col)]
+#change in observables
+chvars=[col for col in ms_data.columns if ('exp' not in col)&('ch' in col)]
+chvars.remove('children')
+chvars.remove('lastchildren')
+#timetrends 
+timevars=['timetrend', 'const']+['const'+str(i) for i in range(0, 15)]
+#samplevars
+samplevars=['samplemain', 'sample2', 'sample2a', 'sample3', 'sample4', 'sample', 'sample4l2i']
+#other variables 
+othervars=[col for col in ms_data.columns if col not in observables+meta+treatment+chexpvars+lastvars+chvars+expvars+timevars+samplevars]
+
+#* Dropping Zone
+#drop othervars - not of interest 
+ms_data=ms_data.drop(othervars, axis=1)
+
+#* Create squared variables 
+ms_data['AGE_sq']=ms_data['AGE']**2
+ms_data['chFAM_SIZE']=ms_data['chchildren']+ms_data['chadults']
+ms_data['chFAM_SIZE_sq']=ms_data['chFAM_SIZE']**2
+
+#* Dummies for Categoricals 
+#categorical variables 
+categoricals=['CKBK_CTX', 'MARITAL1', 'SAVA_CTX', 'CUTENURE']
+#transform categoricals into dummies 
+cats=ms_data[categoricals+['newid']]
+cats_dummies=create_dummies(cats, 'newid')
 
 #* Turn categoricals into two categories only 
 #create dummies for categoricals that have several categories making a distinction between only two broader categories 
-#married=1 if actually married, all other statuses =0
-cleaned['married']=[int(x) for x in cleaned['maritalstat']==1]
+cats_single=cats.copy()
+#married=1 if actually married, all other statuses=0
+cats_single['married']=[int(x) for x in cats_single['MARITAL1']==1]
 #owned_nm=1 if housing owned w/o mortgage 
-cleaned['owned_nm']=[int(x) for x in cleaned['CUTENURE']==2]
+cats_single['owned_nm']=[int(x) for x in cats_single['CUTENURE']==2]
 #owned_m=1 if housing owned w/ mortgage 
-cleaned['owned_m']=[int(x) for x in cleaned['CUTENURE']==1]
+cats_single['owned_m']=[int(x) for x in cats_single['CUTENURE']==1]
 #notowned=1 if housing not owned (all other CUTENURE categories)
-cleaned['notowned']=[int(x) for x in (cleaned['CUTENURE']!=1)&(cleaned['CUTENURE']!=2)]
+cats_single['notowned']=[int(x) for x in (cats_single['CUTENURE']!=1)&(cats_single['CUTENURE']!=2)]
+#then drop the original vars 
+cats_single=cats_single.drop(categoricals, axis=1)
 
-#* Create Dummies for Categoricals 
-#first get subset of data that contains categoricals 
-categoricals=['totbalance_ca', 'maritalstat', 'totbalance_sa', 'CUTENURE']
-cats=cleaned[['newid']+categoricals]
-#then create dummies for each of these 
-cats_dummies=create_dummies(cats, 'newid')
-#merge the dummies back to original data and drop the original variables 
-cleaned=cleaned.drop(categoricals, axis=1)
-cleaned_w_dummies=cleaned.merge(cats_dummies, on='newid')
+#* Individual level dummies 
+#create a df with individual level dummies 
+ind_dums=ms_data[['custid', 'newid']]
+ind_dums=create_dummies(ind_dums, 'custid')
 
 #* Create quarter identifier 
 #t dimension is quarter 
+timedata=ms_data[meta]
 #need a running count from first quarter in data to last one 
-cleaned_w_dummies['quarter']=1
-for i in range(len(cleaned_w_dummies)): 
-    year=cleaned_w_dummies.loc[i, 'year']
-    month=cleaned_w_dummies.loc[i, 'month']
+timedata['quarter']=1
+for i in range(len(timedata)): 
+    year=timedata.loc[i, 'QINTRVYR']
+    month=timedata.loc[i, 'QINTRVMO']
     if year==2008:
         if month in [1, 2, 3]: 
-            cleaned_w_dummies.loc[i, 'quarter']=2
+            timedata.loc[i, 'quarter']=2
         elif month in [4, 5, 6]: 
-            cleaned_w_dummies.loc[i, 'quarter']=3
+            timedata.loc[i, 'quarter']=3
         elif month in [7, 8, 9]: 
-            cleaned_w_dummies.loc[i, 'quarter']=4
+            timedata.loc[i, 'quarter']=4
         elif month in [10, 11, 12]: 
-            cleaned_w_dummies.loc[i, 'quarter']=5
+            timedata.loc[i, 'quarter']=5
     elif year==2009: 
-        cleaned_w_dummies.loc[i, 'quarter']=6
+        timedata.loc[i, 'quarter']=6
 
-#* Panel structure with lags
-#count how often household is observed
-count_obs=cleaned_w_dummies[['custid', 'newid']].groupby('custid').count().merge(cleaned_w_dummies[['custid', 'quarter']], on='custid').rename(columns={'newid': 'count'})
-#can only use observations that have been observed at least 2 times 
-cleaned_w_dummies=cleaned_w_dummies.merge(count_obs[['custid', 'count']], on='custid').drop_duplicates(['newid']).reset_index(drop=True)
-panel=cleaned_w_dummies[cleaned_w_dummies['count']>1]
-#lags 
-#only need lag of treatments and outcomes 
-lag_vars=[col for col in panel.columns if 'exp' in col]+[col for col in panel.columns if 'RBT' in col]
+#* Create dummies for samples 
+#complete sample 
+ms_data['comp_samp']=1
+#sample with no NA 
+ms_data['nona_samp']=ms_data['newid'].isin(ms_data.dropna()['newid']).astype(int)
+#liquidity 
+ms_data['l_samp']=gen_samp_dummy(ms_data, 'liqassii', 'newid')
+#mortgage sample 
+ms_data['mort_samp']=gen_samp_dummy(ms_data, 'ORGMRTX', 'newid')
+#mortgage and liquidity 
+ms_data['l_mort_samp']=(ms_data['mort_samp']==ms_data['l_samp']).astype(int)
 
-#then create lags
-for var in lag_vars: 
-    panel[var+'_lag']=panel[['custid', var]].groupby('custid').shift(-1)
-#drop the 'last' variables 
-panel_dropped=panel.drop([col for col in panel.columns if 'last' in col], axis=1)
-#now only keep households that are only observed 3 times
-panel_lags=panel_dropped[panel_dropped['count']==3]
-#then remove rows that NaN in rows of lag
-panel_nona_lags=panel_lags.dropna(subset=[col for col in panel_lags.columns if '_lag' in col])
+#* Merge everything into one dataset
+final=ms_data.merge(cats_single, on='newid')
+#drop categoricals 
+final=final.drop(categoricals, axis=1)
+final=final.merge(cats_dummies, on='newid')
+final=final.merge(timedata[['newid', 'quarter']], on='newid')
 
-#* Generate MS dataset (only containing their controls)
-ms_data_reduced=ms_data[['custid', 'chTOTexp', 'RBTAMT','AGE', 'chchildren', 'chadults']+['const'+str(i) for i in range(0, 15)]]
-ms_data_reduced['chFAM_SIZE']=ms_data_reduced['chchildren']+ms_data_reduced['chadults']
-ms_data_reduced=ms_data_reduced.drop(['chchildren', 'chadults'], axis=1)
-ms_data_reduced['sqAGE']=ms_data_reduced['AGE']**2
-ms_data_reduced['sqchFAM_SIZE']=ms_data_reduced['chFAM_SIZE']**2
-
-#* Write to CSV
-ms_data_reduced.to_csv(data_out/'transformed'/'ms_setup.csv', index=False)
-cleaned_w_dummies.to_csv(data_out/'transformed'/'cleaned_dummies.csv', index=False)
-panel.to_csv(data_out/'transformed'/'panel_cleaned.csv', index=False)
-panel_nona_lags.to_csv(data_out/'transformed'/'panel_w_lags.csv', index=False)
-print('Finished!')
+final.to_csv(data_out/'transformed'/'prepped_data.csv', index=False)
